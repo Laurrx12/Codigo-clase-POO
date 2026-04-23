0a1c5e5e42a7b1ad896f7febecf3461b87892100
GabsAri <2020gabrielaarias@gmail.com>
2026-04-23 08:17:26 -0500

    Refactor game logic and adjust enemy attributes
    Refactor Mario and Luigi classes, update enemy health.

--- Juego.java
+++ Juego.java
@@ -1,6 +1,56 @@
 import java.util.ArrayList;
 import java.util.Random;
 
+// CLASE PRINCIPAL (PRIMERO)
+public class MarioGame {
+    public static void main(String[] args) {
+
+        ArrayList<Personaje> jugadores = new ArrayList<>();
+        jugadores.add(new Mario());
+        jugadores.add(new Luigi());
+
+        Enemigo goomba = new Enemigo("Goomba");
+        Random rand = new Random();
+
+        System.out.println("🍄 INICIA EL JUEGO DE MARIO 🍄");
+
+        int turno = 1;
+
+        while (goomba.estaVivo()) {
+            System.out.println("\n--- TURNO " + turno + " ---");
+
+            for (Personaje p : jugadores) {
+
+                if (!p.estaVivo()) continue;
+                if (!goomba.estaVivo()) break;
+
+                if (rand.nextBoolean()) {
+                    p.recogerMoneda();
+                }
+
+                if (p instanceof PoderEspecial && rand.nextBoolean()) {
+                    ((PoderEspecial) p).usarPoder();
+                }
+
+                p.atacar(goomba);
+
+                System.out.println("Vida del " + goomba.getTipo() + ": " + goomba.getVida());
+
+                if (goomba.estaVivo()) {
+                    goomba.atacar(p);
+                    System.out.println(p.getNombre() + " vida: " + p.getVida());
+                }
+
+                System.out.println("-------------------");
+            }
+
+            turno++;
+        }
+
+        System.out.println("🏁 Fin del juego");
+    }
+}
+
 // INTERFACE
 interface PoderEspecial {
     void usarPoder();
@@ -32,20 +82,28 @@ abstract class Personaje {
         return vida > 0;
     }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
     public abstract void atacar(Enemigo e);
 }
 
-// CLASE MARIO
+// MARIO
 class Mario extends Personaje implements PoderEspecial {
-    private boolean tieneEstrella = false;
+    private int turnosEstrella = 0;
 
     public Mario() {
         super("Mario");
     }
 
     public void activarEstrella() {
-        tieneEstrella = true;
-        System.out.println("⭐ Mario es invencible!");
+        turnosEstrella = 2;
+        System.out.println("⭐ Mario es invencible por 2 turnos!");
     }
 
     @Override
@@ -55,18 +113,20 @@ class Mario extends Personaje implements PoderEspecial {
 
     @Override
     public void atacar(Enemigo e) {
-        if (tieneEstrella) {
-            System.out.println("Mario destruye al enemigo con estrella ⭐");
-            e.recibirDano(100);
+        if (turnosEstrella > 0) {
+            System.out.println("⭐ Mario destruye al enemigo!");
+            e.recibirDano(50);
+            turnosEstrella--;
         } else {
             System.out.println("Mario salta sobre el enemigo 🦘");
-            e.recibirDano(30);
+            e.recibirDano(20);
         }
     }
 }
 
-// CLASE LUIGI
+// LUIGI
 class Luigi extends Personaje {
+
     public Luigi() {
         super("Luigi");
     }
@@ -85,12 +145,17 @@ class Enemigo {
 
     public Enemigo(String tipo) {
         this.tipo = tipo;
-        this.vida = 60;
+        this.vida = 80;
     }
 
     public void recibirDano(int dano) {
         vida -= dano;
         if (vida < 0) vida = 0;
+    }
+
+    public void atacar(Personaje p) {
+        System.out.println(tipo + " ataca a " + p.getNombre() + " 👾");
+        p.recibirDano(15);
     }
 
     public boolean estaVivo() {
@@ -103,43 +168,5 @@ class Enemigo {
 
     public int getVida() {
         return vida;
-    }
-}
-
-// JUEGO PRINCIPAL
-public class MarioGame {
-    public static void main(String[] args) {
-
-        ArrayList<Personaje> jugadores = new ArrayList<>();
-        jugadores.add(new Mario());
-        jugadores.add(new Luigi());
-
-        Enemigo goomba = new Enemigo("Goomba");
-
-        Random rand = new Random();
-
-        System.out.println("🍄 INICIA EL JUEGO DE MARIO 🍄");
-
-        for (Personaje p : jugadores) {
-
-            if (!goomba.estaVivo()) break;
-
-            // Probabilidad de moneda
-            if (rand.nextBoolean()) {
-                p.recogerMoneda();
-            }
-
-            // Probabilidad de poder especial
-            if (p instanceof PoderEspecial && rand.nextBoolean()) {
-                ((PoderEspecial) p).usarPoder();
-            }
-
-            p.atacar(goomba);
-
-            System.out.println("Vida del " + goomba.getTipo() + ": " + goomba.getVida());
-            System.out.println("-------------------");
-        }
-
-        System.out.println("🏁 Fin del juego");
     }
 }
